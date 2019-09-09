66409dbb1e1a12f79fcc8a5ec111c429ec4b10a4
Arjan Stam <arjan.stam@hotmail.com>
2019-09-09 14:37:54 +0200

    lab1 en 2

---
+++ labs/lab 1/js/script.js
@@ -0,0 +1,12 @@
+alert("U dient 3 vragen te beantwoorden")
+
+var test = prompt("Wat is uw voornaam?");
+var test1 = prompt("Wat is uw achternaam?");
+var test2 = prompt("Wat is uw leeftijd?");
+
+document.write(test); // slaat voornaam op als variabele
+document.write("<br/>"); // witregel
+document.write(test1); // slaat achternaam op als variabele
+document.write("<br/>"); // witregel
+document.write(test2); // slaat leeftijd op als variabele
+document.write("<br/>") // witregel
