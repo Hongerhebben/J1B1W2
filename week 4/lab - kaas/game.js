851889b1aef441621e058e8b604067656243b673
Arjan Stam <arjan.stam@hotmail.com>
2019-09-24 12:40:34 +0200

    ..

---
+++ week 4/lab - kaas/game.js
@@ -0,0 +1,86 @@
+const requirement = alert ("welkome bij de game");
+const mesage1 = alert ("je moet alle vragen met A of B beantwoorden");
+const mmesage1 = alert ("begin van de game");
+const  mesage2 = alert("je word wakker en lig lekker in bed");
+const mesage3 =("je bent een beetje moe maar je moet uit bed");
+
+if (bed == "A"){
+        const mesage1a = alert("je gaat naar beneden")
+        const mesage2a = alert("wat voor ondbijt neem je deze ochtend")
+        var masage3a = prompt("A salade B pizza")
+
+        if (masage3a == "A") {
+            document.write("je stikte in een fucking blaadje sla")}
+            }
+
+        if (masage3a == "B"){
+            const mesage3a1 = alert("zul hem in de oven of de magnatron doen")
+            var mesage3a2 = prompt("A oven B magnatron")
+            console.log("DE OVEN NAATUURLIJK")
+            }
+            if (mesage3a2 == "A") {
+                const mesage3a2a1 = alert("Na een halfuurtje wachten heb je een heerlijke pizza op")
+                const mesage3a2a2 = alert("Je stapt de deur uit")
+                const mesage3a2a3 = alert("hoe zou je nu naar school gaan")
+                var mesage3a2a4 = prompt("A de bus B de trein C lopend")
+                if (mesage3a2a4 == "A") {
+                    const debus1 = alert("Je stapt in de bus en gaat zitten") 
+                    const debus2 = alert("je gaat zitten en ineens hoor je naast je mimimimi")
+                    var debus3 = prompt("A REN B spreek die persoon aan dat ie zijn bek moet houden C blijf stil")
+                }
+                    if (debus3 == "A") {
+                    const debus3a1 = alert("je sprint weg als een mothafocka")
+
+                    }
+                    if (debus3 == "B"){
+                    document.write("je bent verkracht in de bus door boer harm")
+                    }
+                    if (debus3 == "C"){
+                    document.write("je bent verkracht in de bus door boer harm")
+                    }
+                    }
+
+
+                if (mesage3a2a4 == "B") {
+                    console.log("de trein in boy")
+                    const detrein9 = alert("Je stapt in de trein")
+                    const detrein2 = alert("je ziet het model van de trein op de muur staan")
+                    const detrein3 = alert("het is een Matrein")
+                    const detrein4 = alert("en toen plotseling ontspoorde Matrein")
+                    const detrein5 = alert("je stapte uit de trein en dacht fuck school ik heb trek in")
+                    var detrein6 = prompt("A macdonalds B KFC")
+                    }
+                    if (detrein6 == "A") {
+
+                    document.write("Je ging naar de mac en toen je een hap nam ging je dood door diabetes");
+                    }
+                    if (detrein6 == "B"){
+                    document.write("Je ging naar de KFC en had een over heerlekijk maaltijd en toen je van de laatste hap genoot was je daar, inees in de hemel bij de almachtige colonel Sanders, tachanka")
+
+                }
+                if (mesage3a2a4 == "C") {
+                    document.write("je werd verkracht in de bosjes")
+
+                }
+
+            if (mesage3a2 == "B") { document.write("je kreeg kanker and you fucking died")
+
+                }
+
+
+if (bed == "B"){
+        const mesage1b = alert("ow oke dat kan ook blijf maar lekker in bed liggen")
+        const mesage2b =  alert("Je moeder komt boos naar boven en zegt dat je naar school moet")
+        var mesage3b = prompt("a stap uit bed / b ben ziek ")
+        }
+        if (mesage3b == "A") {
+            document.write("je viel van de trap en je breekte je nek")
+
+        }
+
+
+
+        if (mesage3b == "B") {
+            const mesage3b1b = alert("m: ok")
+            document.write("Sleepy ending")
+            }
---
+++ week 4/lab - kaas/kaas.js
@@ -0,0 +1,78 @@
+var geel = prompt("Is de kaas geel?");
+
+if(geel == "ja"){
+
+var gaten = prompt("Zitten er gaten in?");
+
+}
+
+if(geel == "nee"){
+
+var schimmels = prompt("heeft de kaas blauwe schimmels?");
+
+}
+
+if(schimmels == "nee"){
+
+var korst = prompt("Heeft de kaas een korst?");
+
+}
+
+if(korst == "nee"){
+
+alert("Uw kaas is Mozarella." )
+
+}
+if(korst == "ja"){
+
+alert("Uw kaas is Camembert")
+
+}
+
+if(schimmels == "ja"){
+
+
+var korst = prompt("Heeft de kaas een korst?")
+
+}
+
+if(korst == "ja"){
+
+alert("Uw kaas is Bleu de Rochbaron")
+
+}
+if(korst == "nee"){
+
+alert("Uw kaas is Founne d'Ambert")
+
+}
+if(gaten == "ja"){
+
+var duur = prompt("is de kaas belachelijk duur?")
+
+}
+if(gaten =="nee"){
+
+var steen = prompt("is de kaas zo hard als steen?")
+
+}
+if(duur =="ja"){
+
+alert("Uw kaas is Emmenthaler")
+
+}
+if(duur =="nee"){
+
+alert("Uw kaas is Leerdammer")
+
+}
+if(steen =="ja"){
+
+alert("Uw kaas is Pannigiano Reggiano")
+
+}
+if(steen =="nee"){
+
+alert("Uw kaas is Goudse kaas")
+
+}
