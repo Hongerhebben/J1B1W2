a5d504795e3d8dd893049c0f0d27ca4e4e726338
Arjan Stam <arjan.stam@hotmail.com>
2019-09-30 09:11:08 +0200

    game

---
+++ week 4/lab - kaas/gamee.js
@@ -0,0 +1,83 @@
+const requirement = alert ("welkome bij de game");
+const mesage1 = alert ("je moet alle vragen met 1 of 2 beantwoorden");
+const mmesage1 = alert ("De game begintt..");
+const  mesage2 = alert("Je ligt in bed en je droomt");
+const mesage3 = alert("Je droomt over school en dat er brand is, jij bent de enigste die het ziet maar wat doe je?");
+var bed = prompt("1 Ren je uit school zonder iemand te vertellen dat er brand is / 2 druk op het brandalarm en ren roepend langs alle klaslokalen. ")
+if (bed == "1"){
+        const mesage1a = alert("Je rent de school uit")
+        const mesage2a = alert("Je komt langs een brandweerkazerne")
+        const mesagee3a = alert("Maar wat doe je?")
+        var masage3a = prompt("1 Waarschuw de brandweer dat er brand is, 2 ren er voorbij en doe niks")}
+        if (masage3a == "1"){
+        	const brandweer1 = alert("Je waarschuwt de brandweer.")
+        	const brandweer2 = alert("De brandweer zegt dat ze geen tijd hebben en dat je moet oprotten.")
+        	const brandweer3 = alert("Je gaat huilend weg en de school brand hevig door.")
+        	const brandweer4 = alert("Je rent voorbij een rivier waar een emmer staat, maar wat doe je?")
+        	var brandweer0 = prompt("1 Pak de emmer, vul hem met water en ren naar de school/ 2 Je hebt het warm en springt het water in.")}
+        	if (brandweer0 == "1"){
+        	document.write("Je rent met de emmer richting de school, je struikelt en valt flauw.")}
+        	if (brandweer0 == "2"){
+        	document.write("Je springt in het water en verdrinkt...")}
+        if (masage3a == "2"){
+        	const rennen1 = alert("Je rent voorbij de brandweer");
+        	const rennen2 = alert("Je ziet een snackbar");
+        	const rennen3 = alert("Ga je wel of niet de snackbar in?");
+        	var snackbar0 = prompt("1 Je gaat naar de snackbar, 2 Je besluit om honger te lijden")}
+        	if (snackbar0 == "1"){
+        	const honger4 = alert("Wat ga je bestellen?")
+        	var patat0 = prompt("1 een patatje met, 2 een frikanel speciaal.")}
+        	if (patat0 == "1"){
+        	const eten1 = alert("Je bestelt een patatje met.")
+        	const eten2 = alert("maar dan.........")
+        	const eten3 = alert("Je begint het op te eten")
+        	document.write("helaas.. je bent gestikt in een patatje")}
+
+        	if (snackbar0 == "2"){
+        	const honger1 = alert("Je gaat verder zonder wat te eten :(");
+        	const honger2 = alert("Je gaat twijfelen om toch wat te gaan bestellen, omdat je toch best wel veel honger begint te krijgen.");
+        	const honger3 = alert("Je besluit toch terug te gaan en naar de snackbar te gaan. Je wilt iets bestellen.")
+        	const honger4 = alert("Wat ga je bestellen?")
+        	var patat1 = prompt("1 een patatje met, 2 een frikanel speciaal.")}
+        	if (patat1 == "1"){
+        	const eten4 = alert("Je bestelt een patatje met.")
+        	const eten5 = alert("maar dan.........")
+        	const eten6 = alert("Je begint het op te eten")
+        	document.write("helaas.. je bent gestikt in een patatje")}
+        	if (patat1 == "2"){
+        		const frikanel = alert("Je bestelt 10 frikandellen")
+        		const frikanel1 = alert("Je eet alles op, maar dan..")
+        		document.write("helaas.. je bent gestikt in een frikandel")}
+
+      //2e kant
+if (bed == "2"){
+	const ren = alert("Je rent door de school en iedereen rent snel naar buiten")
+	var ren1 = prompt("Nu iedereen toch buiten is wil je wat anders doen dan school, maar wat ga je doen? 1 ga naar de bios 2 ga naar de cafetaria")}
+	if (ren1 == "1"){
+		var bios = prompt("Je gaat naar de bios maar naar welke film ga je? 1 The Lion King, 2 IT chapter two")}
+		if (bios == "1"){
+			var pop = prompt("Je gaat naar The Lion King, maar wat voor popcorn neem je in de bios? 1 Popcorn zout 2 Popcorn zoet")}
+			if (pop == "1"){
+				const popcorn1 = alert("je koos zoute popcorn, maar dan..")
+				document.write("Je bent gestikt in een popcorntje")}
+			if (pop == "2"){
+				const popcorn2 = alert("je koos zoete popcorn, maar dan..")
+				document.write("Je bent gestikt in een popcorntje")}
+		if (bios == "2"){
+			var pop2 = prompt("Je gaat naar IT chapter two, maar wat voor popcorn neem je in de bios? 1 Popcorn zout 2 Popcorn zoet")}
+			if (pop2 == "1"){
+				const popcorn1 = alert("je koos zoute popcorn, maar dan..")
+				document.write("Je bent gestikt in een popcorntje")}
+			if (pop2 == "2"){
+				const popcorn2 = alert("je koos zoete popcorn, maar dan..")
+				document.write("Je bent gestikt in een popcorntje")}
+
+	if (ren1 == "2"){
+		const ren2 = alert("Je gaat naar de cafetaria")
+		var ren3 = prompt("Maar wat ga je bestellen? 1 niks 2 patat")}
+		if (ren3 == "1"){
+			const niks = alert("Je besteld niks en vindt dat niet leuk, maar dan..")
+			document.write("Gefeliciteerd! Je werd wakker")}
+		if (ren3 == "2"){
+			const patat2 = alert("Je eet je patat, maar dan....")
+			document.write("Helaas.. Je bent gestikt in een frietje.")}
